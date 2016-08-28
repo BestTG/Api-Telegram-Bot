24dd03406e6fe843f0c21966fe097d84dca95759
BestTG <infonewlookteam@gmail.com>
2016-08-28 07:45:54 -0800

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = " "--توکن بوت را درون " قرار دهید --
+local bot_api_key = "263250676:AAG67lQtXc7IhnMg3g8FwPgWkdHYTgzsWvg"--توکن بوت را درون " قرار دهید --
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[
@@ -54,7 +54,7 @@ _TNX TO USE ME :)_
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {94746365}-- put your id here
+  local admins = {32347781}-- put your id here
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
@@ -254,7 +254,7 @@ function msg_processor(msg)
 
 elseif msg.text:match("^/[sS]tart") or msg.text:match("^/[Hh]elp") then
  sendMessage(msg.chat.id, start, true, false, true)
-  sendMessage(msg.chat.id, [[[ربات خودتونو بسازید :)](http://telegram.me/beatbot_team)]], true, false, true)
+  sendMessage(msg.chat.id, [[[ربات خودتونو بسازید :)](http://telegram.me/PmResan)]], true, false, true)
 return end
 
 end
